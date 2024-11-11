230d37ecc99c8967eb93df6e7ce176ba05a3b2a3
carollousdev <carollousdev@gmail.com>
2024-11-12 02:52:34 +0700

    Setting routes and controllers

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
+$routes->get('/coba/index/', 'Coba::index');
+$routes->get('/coba/about/', 'Coba::about');
+$routes->get('/coba/(:any)(:num)', 'Coba::about/$1/$2');
+
+$routes->get('/users/', 'Admin\Users::index');
